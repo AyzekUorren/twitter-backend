44a7c1f2cebd664ba08c15fc0e0a30e69367ffb6
AyzekUorren <proganatoliy@gmail.com>
2019-03-30 21:27:15 +0200

    add: db url to procces.env

--- src/database/database.providers.ts
+++ src/database/database.providers.ts
@@ -1,9 +1,10 @@
 import * as mongoose from 'mongoose';
+const mongourl = process.env.DB_URL || `mongodb://localhost/twitter-app-backend`;
 
 export const databaseProviders = [
     {
         provide: 'DATABASE_CONNECTION',
         useFactory: async (): Promise<typeof mongoose> =>
-            await mongoose.connect('mongodb://localhost/twitter-app-backend'),
+            await mongoose.connect(mongourl),
     },
 ];
