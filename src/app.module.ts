6153d6925994c07b998d641daa271c85ce85aa10
AyzekUorren <proganatoliy@gmail.com>
2019-03-30 21:48:48 +0200

    update mongo config

--- src/app.module.ts
+++ src/app.module.ts
@@ -8,13 +8,13 @@ import { MongooseModule } from '@nestjs/mongoose';
 import { UserModule } from './user/user.module';
 import { DatabaseModule } from './database/database.module';
 import { AuthController } from './auth/auth.controller';
-import { userProviders } from './user/user.providers';
 import { AuthModule } from './auth/auth.module';
 import { JwtStrategy } from './auth/jwt.strategy';
 
+const mongourl = process.env.DB_URL || `mongodb://test:qqqqqq1@ds229186.mlab.com:29186/twitter-backend`;
 @Module({
   imports: [
-    MongooseModule.forRoot('mongodb://localhost/twitter-app-backend'),
+    MongooseModule.forRoot(mongourl, { useNewUrlParser: true }),
     UserModule,
     DatabaseModule,
     AuthModule,
@@ -28,7 +28,6 @@ import { JwtStrategy } from './auth/jwt.strategy';
     AppService,
     UserService,
     AuthService,
-    ...userProviders,
     JwtStrategy,
   ],
 })
--- src/database/database.providers.ts
+++ src/database/database.providers.ts
@@ -1,10 +1,10 @@
 import * as mongoose from 'mongoose';
-const mongourl = process.env.DB_URL || `mongodb://localhost/twitter-app-backend`;
+const mongourl = process.env.DB_URL || `mongodb://test:qqqqqq1@ds229186.mlab.com:29186/twitter-backend`;
 
 export const databaseProviders = [
     {
         provide: 'DATABASE_CONNECTION',
         useFactory: async (): Promise<typeof mongoose> =>
-            await mongoose.connect(mongourl),
+            await mongoose.connect(mongourl, { useNewUrlParser: true }),
     },
 ];
