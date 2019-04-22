b78208f8777b94bd3605d5a1c5fb2509bb12d5bf
AyzekUorren <proganatoliy@gmail.com>
2019-04-23 01:37:00 +0300

    update config

--- src/database/database.providers.ts
+++ src/database/database.providers.ts
@@ -1,10 +1,10 @@
 import * as mongoose from 'mongoose';
-const mongourl = process.env.DB_URL || `mongodb://test:qqqqqq1@ds229186.mlab.com:29186/twitter-backend`;
+import { ConfigService } from 'src/config/config.service';
 
 export const databaseProviders = [
     {
         provide: 'DATABASE_CONNECTION',
-        useFactory: async (): Promise<typeof mongoose> =>
-            await mongoose.connect(mongourl, { useNewUrlParser: true }),
+        useFactory: async (config: ConfigService): Promise<typeof mongoose> =>
+            await mongoose.connect(config.get('MONGO_URL'), { useNewUrlParser: true }),
     },
 ];
