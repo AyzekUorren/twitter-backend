e6bb9e62bb3e3164f7b799321896103bea844e81
Anatoliy Pogoriliy <anatoliy.pogoriliy@mev.com>
2019-04-23 12:39:28 +0300

    config update

--- src/main.ts
+++ src/main.ts
@@ -1,14 +1,16 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { Logger } from '@nestjs/common';
 
 async function bootstrap() {
   const port = +process.env.PORT || 3000;
+  const apiPrefix = process.env.API_PREFIX || 'dev';
   const app = await NestFactory.create(AppModule, {
     bodyParser: true,
   });
-  app.setGlobalPrefix('v1');
+  app.setGlobalPrefix(apiPrefix);
   await app.listen(port, () => {
-    console.log(`-> Listening on	\x1b[34m http://localhost:${ port }\x1b[0m`);
+    Logger.log(`-> Listening on	\x1b[34m http://localhost:${ port }/${ apiPrefix }\x1b[0m`);
     // console.log(`-> Api docs on	\x1b[32m http://${ host }:${ port }/api/v1/docs\x1b[0m`);
   });
 }
