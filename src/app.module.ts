ea5ed5223a9117a3703620ed0ab62cd3e9fb68ba
AyzekUorren <proganatoliy@gmail.com>
2019-04-26 02:31:55 +0300

    add: userModule

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,14 +1,14 @@
+import { UserModule } from './user/user.module';
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { MongooseModule } from '@nestjs/mongoose';
 import { DatabaseModule } from './database/database.module';
-import { ConfigService } from './config/config.service';
 import { ConfigModule } from './config/config.module';
 @Module({
   imports: [
     ConfigModule,
     DatabaseModule,
+    UserModule,
   ],
   controllers: [
     AppController,
