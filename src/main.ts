068a35d622ded6e62865dddde2f01058c6dfb885
Anatoliy Pogoriliy <proganatoliy@gmail.com>
2019-05-03 17:06:31 +0300

    hotfix: return for config

--- src/main.ts
+++ src/main.ts
@@ -3,18 +3,18 @@ import { NestFactory } from '@nestjs/core';
 import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 import { AppModule } from './app.module';
 
-async function bootstrap() {
+async function bootstrap (){
   let app;
   const port = +process.env.PORT || 3000;
   if (process.env.NODE_ENV === 'production') {
     app = await NestFactory.create(AppModule, {
       logger: console,
-      bodyParser: true
+      bodyParser: true,
     });
   }
   else {
     app = await NestFactory.create(AppModule, {
-      bodyParser: true
+      bodyParser: true,
     });
   }
 
@@ -37,7 +37,7 @@ async function bootstrap() {
   await app.listen(port, () => {
     if (process.env.NODE_ENV !== 'production') {
       Logger.log(
-        `-> Listening on	\x1b[34m http://localhost:${port}/docs\x1b[0m`
+        `-> Listening on	\x1b[34m http://localhost:${port}/docs\x1b[0m`,
       );
     }
   });
