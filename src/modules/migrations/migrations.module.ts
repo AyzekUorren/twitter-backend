c7ed9d264f178e4540a270fabe1a700891e7125b
AyzekUorren <proganatoliy@gmail.com>
2019-05-11 23:58:05 +0300

    move: migrations to modules

--- src/modules/migrations/migrations.module.ts
+++ src/modules/migrations/migrations.module.ts
@@ -1,9 +1,9 @@
 import { MigrationProviders } from './migrations.providers';
 import { MigrationService } from './migrations.service';
 import { MigrationController } from './migrations.controller';
-import { ConfigModule } from './../config/config.module';
+import { ConfigModule } from '../../config/config.module';
 import { Module } from '@nestjs/common';
-import { DatabaseModule } from '../database/database.module';
+import { DatabaseModule } from '../../database/database.module';
 
 @Module({
     imports: [DatabaseModule, ConfigModule],
